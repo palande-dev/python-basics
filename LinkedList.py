cee185285d9f39c18cc1b9cfee402a3f138340f6
shweta palande <shweta palande>
2017-04-17 23:22:26 -0700

    Append elements at the end and insert at kth position

--- LinkedList.py
+++ LinkedList.py
@@ -2,7 +2,7 @@
 
 
 
-class LinkedList():                             
+class LinkedList:                             
   
   def __init__(self):
     self.head = None
@@ -16,7 +16,9 @@ class LinkedList():
         self.head = newNode 
       else:
         newNode.setNext(self.head)
-        self.head = newNode                 
+        self.head = newNode 
+        
+  
     
   def isEmpty(self):
     return self.head == None
@@ -27,23 +29,55 @@ class LinkedList():
       print node.getData()
       node = node.getNext()
       
-''' create class node. it has 2 attributes: data and next. '''
+      
+  def length(self):
+    count = 1
+    curr = self.head
+    while curr.next != None:
+      curr = curr.next
+      count = count +1
+    return count
+    
+  ''' To append data at the end of LinkedList '''  
+  def append(self,data):
+    newADD = Node(data)
+    curr = self.head
+    while curr.next != None:
+      curr = curr.next
+      
+    curr.next = newADD
+    #newADD.next = None
+    
+  ''' To insert data at the kth position in the LinkedList '''
+  def insertAt(self,data,pos):
+    newNode = Node(data)
+    count = 1
+    curr = self.head
+    while curr.next != None and count < pos:
+      count = count + 1
+      if count == pos:
+        newNode.next = curr.next
+        curr.next = newNode
+      curr = curr.next
+    
+   
+
 class Node: 
-  def __init__(self,initdata):
-    self.data = initdata
-    self.next = None
+    def __init__(self,initdata):
+      self.data = initdata
+      self.next = None
     
-  def setData(self,newData):
-    self.data = newData
+    def setData(self,newData):
+      self.data = newData
   
-  def setNext(self, newNext):
-    self.next = newNext
+    def setNext(self, newNext):
+      self.next = newNext
     
-  def getData(self):
-    return self.data
+    def getData(self):
+      return self.data
     
-  def getNext(self):
-    return self.next
+    def getNext(self):
+      return self.next
     
     
 Mylist = LinkedList()
@@ -54,5 +88,13 @@ Mylist.addData(3)
 Mylist.addData(2)
 Mylist.addData(1)
 Mylist.printLinkedList()
-print Mylist.isEmpty()
+#print Mylist.isEmpty()
+print "lenght is ",Mylist.length()
+print Mylist.append(101)
+print "lenght is after appending  ",Mylist.length()
+
+Mylist.printLinkedList()
+Mylist.insertAt(202,3)
+print "lenght is after inserting  ",Mylist.length()
+Mylist.printLinkedList()
     
